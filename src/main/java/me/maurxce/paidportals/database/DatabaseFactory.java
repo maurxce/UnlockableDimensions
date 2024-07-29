1d1a9163f3698b9f6b0b82961455b6cdca0b96d7
Maurice Schorn <pxlmo.codes@gmail.com>
2024-07-29 12:30:24 +0200

    add h2 support (wip)

--- src/main/java/me/maurxce/paidportals/database/DatabaseFactory.java
+++ src/main/java/me/maurxce/paidportals/database/DatabaseFactory.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import md.schorn.spigothelper.configuration.Config;
 import md.schorn.spigothelper.logger.Logger;
 import me.maurxce.paidportals.PaidPortals;
+import me.maurxce.paidportals.database.implementation.H2;
 import me.maurxce.paidportals.database.implementation.MongoDB;
 import me.maurxce.paidportals.database.implementation.MySQL;
 
@@ -13,11 +14,12 @@ public class DatabaseFactory {
 
     public Database setupDatabase() {
         Config config = plugin.getConfig();
-        String type = config.getString("database.type", "mysql");
+        String type = config.getString("database.type", "h2");
 
         return switch (type.toUpperCase()) {
             case "MYSQL" -> new MySQL(plugin);
             case "MONGODB" -> new MongoDB(plugin);
+            case "H2" -> new H2(plugin);
             default -> {
                 Logger.severe("Invalid database configuration!");
                 plugin.disable();
